aa25543fe1e8b3b173591da3a00c21f36f85b6b4
WMBarbosa <wesleymario01@gmail.com>
2024-09-03 17:18:42 -0300

    Atualizando com o ultimo teste

--- src/Faturamento.java
+++ src/Faturamento.java
@@ -4,7 +4,7 @@ import java.util.Map;
 public class Faturamento {
 
     public static void main(String[] args) {
-        // Faturamento mensal por estado
+
         Map<String, Double> faturamento = new HashMap<>();
         faturamento.put("SP", 67836.43);
         faturamento.put("RJ", 36678.66);
@@ -12,13 +12,11 @@ public class Faturamento {
         faturamento.put("ES", 27165.48);
         faturamento.put("Outros", 19849.53);
 
-        // Cálculo do valor total
         double totalFaturamento = 0;
         for (double valor : faturamento.values()) {
             totalFaturamento += valor;
         }
 
-        // Cálculo do percentual de representação e exibição dos resultados
         System.out.println("Percentual de representação por estado:");
         for (Map.Entry<String, Double> entry : faturamento.entrySet()) {
             String estado = entry.getKey();
